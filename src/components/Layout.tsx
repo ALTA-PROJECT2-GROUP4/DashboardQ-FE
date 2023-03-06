24a6b4f43013037f8c55ed99a421667449a77778
Agung Cahya <agungcahya122@gmail.com>
2023-03-06 18:46:01 +0700

    feat(build and instalation):

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="h-screen w-full overflow-auto ">
+      <div className="h-full w-full overflow-auto bg-color1">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        color1: "#232932",
+        color2: "#EBECF1",
+        color3: "#1F4068",
+        color4: "#395B64",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
+};
