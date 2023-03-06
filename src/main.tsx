5efb9a6ffb28d0f773291e1bc7eb5ab7bb417627
fikri amanda <fikriamd12@gmail.com>
2023-03-06 04:12:09 -1200

    feat: add router for UserList

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <UserList />
+    <App />
   </React.StrictMode>
 );
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,21 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
 import LandingPage from "../pages/LandingPage";
 import Dashboard from "../pages/Dashboard";
+import UserList from "../pages/UserList";
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Dashboard />,
-    },
-    {
-      path: "/landingpage",
-      element: <LandingPage />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />}/>
+      <Route path="/landingpage" element={<LandingPage />}/>
+      <Route path="/user" element={<UserList />}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 export default App;
