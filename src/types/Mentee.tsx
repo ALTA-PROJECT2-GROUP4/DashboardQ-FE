23c9f240d6f78e247651374f1aa573dcebd4db2c
Agung Cahya <agungcahya122@gmail.com>
2023-03-09 00:17:03 +0700

    feat: integrasi get all/list class

--- src/types/Mentee.tsx
+++ src/types/Mentee.tsx
@@ -1,8 +1,16 @@
 export interface MenteeType {
-    id?: number;
-    name?: string;
-    class?: string;
-    status?: string;
-    category?: string;
-    gender?: string;
+  id?: number;
+  name?: string;
+  class?: string;
+  status?: string;
+  category?: string;
+  gender?: string;
+}
+
+export interface ClassType {
+  id: number;
+  name: string;
+  start_class: string;
+  end_class: string;
+  mentor: string;
 }
